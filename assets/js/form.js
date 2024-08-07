bda9fd90ff6ccfcaf5915d56782fe1506b641b53
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-07 19:52:36 +0300

    consent label pointer

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,8 @@
 const main = document.querySelector('main');
 const blocks = main.querySelectorAll('.block');
 const submit = main.querySelector('#submit');
+const general = main.querySelector('#general');
+
 
 blocks.forEach(block => {
     block.addEventListener('click', () => {
@@ -10,4 +12,10 @@ blocks.forEach(block => {
 submit.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('submit');
+});
+general.addEventListener('click', (e) => {
+    for (const child of general.children) {
+        console.log(child);
+
+    }
 });
